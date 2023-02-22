ef7656fec167dab1de1d2cbf9dfa9ace109a9f93
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-02-22 18:52:51 +0300

    finish practice

--- classes/teacher.js
+++ classes/teacher.js
@@ -1,7 +1,19 @@
-const Person = require('./person');
+const Person = require("./person");
 
 // Your code here
+class Teacher extends Person {
+  constructor(fistName, lastName, subject, yearsOfExperience) {
+    super(fistName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
 
+  static combinedYearsOfExperience(teachers) {
+    return teachers.reduce((sum, teacher) => {
+      return sum += teacher.yearsOfExperience;
+    }, 0);
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
